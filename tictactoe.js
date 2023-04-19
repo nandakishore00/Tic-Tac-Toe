d42d4d5fde6a2b872b25a30449177b1a56ca7572
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-04-19 23:43:51 +0530

    allow user to start game

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,10 @@
-console.log("helo")
+const btnLight = document.querySelector(".btn-light");
+const center = document.querySelector(".center");
+const start = document.querySelector(".start");
+const starter = document.querySelector(".starter");
+
+btnLight.addEventListener("click", function() {
+    center.style.display = 'flex';
+    start.style.display = 'none';
+    starter.style.height = '0vh';
+})
