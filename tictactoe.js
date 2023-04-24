df3d394b9372d988981a39d1f33eb9df6f79dbc0
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-04-24 15:36:29 +0530

    allow input from 2 players

--- tictactoe.js
+++ tictactoe.js
@@ -2,9 +2,28 @@ const btnLight = document.querySelector(".btn-light");
 const center = document.querySelector(".center");
 const start = document.querySelector(".start");
 const starter = document.querySelector(".starter");
-
+const X_PLAYER='X'
+const O_PLAYER='O'
+let CURRENT_PLAYER=X_PLAYER
 btnLight.addEventListener("click", function() {
     center.style.display = 'flex';
     start.style.display = 'none';
     starter.style.height = '0vh';
 })
+const spaces = new Array(9).fill(0);
+const cells = document.querySelectorAll(".cell")
+    cells.forEach((cell)=>{
+        cell.addEventListener("click",function(e){
+            if(!e.target.id)
+            {
+                e.target.innerHTML=CURRENT_PLAYER
+                if(CURRENT_PLAYER==X_PLAYER){
+                    CURRENT_PLAYER=O_PLAYER
+                }
+                else{
+                    CURRENT_PLAYER=X_PLAYER
+                }
+            }
+           
+        })
+    })
