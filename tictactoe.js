96123b0ecaed700e06f59dd635543a687de7640d
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-04-26 19:01:18 +0530

    restricts user from editing thier input

--- tictactoe.js
+++ tictactoe.js
@@ -2,6 +2,7 @@ const btnLight = document.querySelector(".btn-light");
 const center = document.querySelector(".center");
 const start = document.querySelector(".start");
 const starter = document.querySelector(".starter");
+const spaces = new Array(9).fill(0);
 const X_PLAYER='X'
 const O_PLAYER='O'
 let CURRENT_PLAYER=X_PLAYER
@@ -10,11 +11,11 @@ btnLight.addEventListener("click", function() {
     start.style.display = 'none';
     starter.style.height = '0vh';
 })
-const spaces = new Array(9).fill(0);
 const cells = document.querySelectorAll(".cell")
     cells.forEach((cell)=>{
         cell.addEventListener("click",function(e){
-            if(!e.target.id)
+            console.log(e.target)
+            if(e.target.innerHTML=='')
             {
                 e.target.innerHTML=CURRENT_PLAYER
                 if(CURRENT_PLAYER==X_PLAYER){
