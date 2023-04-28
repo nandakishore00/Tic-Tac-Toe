2bcfd43c1c53000428aa5e2dca136f09e5883b85
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-04-28 21:40:29 +0530

    doesn't let player register any input after winning the match

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,12 @@
 const btnLight = document.querySelector(".btn-light");
+var header=document.querySelector(".heading")
 const center = document.querySelector(".center");
 const start = document.querySelector(".start");
 const starter = document.querySelector(".starter");
 const restartBtn=document.querySelector(".restart");
 const cells = document.querySelectorAll(".cell")
 var spaces = new Array(9).fill(null);
+var gameOver=false
 const X_PLAYER='X'
 const O_PLAYER='O'
 let CURRENT_PLAYER='X'
@@ -14,39 +16,48 @@ start.addEventListener("click", function() {
     start.style.display = 'none';
     starter.style.height = '0vh';
 })
+restartBtn.addEventListener("click",restart)
 cells.forEach((cell)=>{
     cell.addEventListener("click",function(e){
-        id=e.target.id
-        if(spaces[id]==null)
-        {   spaces[id]=CURRENT_PLAYER
-            count+=1
-            const playerWon=playerHasWon()
-            if(playerWon==true){
+        if(gameOver==false){
+            id=e.target.id
+            if(spaces[id]==null)
+            {   spaces[id]=CURRENT_PLAYER
+                count+=1
+                const playerWon=playerHasWon()
+                if(playerWon==true){
+                    e.target.innerHTML=CURRENT_PLAYER
+                    gameOver=true
+                    if(CURRENT_PLAYER=='X'){
+                        header.innerHTML="PLAYER 1 WON"
+                    }
+                    else{
+                        header.innerHTML="PLAYER 2 WON"
+                    }
+                    e.preventDefault()
+                }
                 e.target.innerHTML=CURRENT_PLAYER
-                e.preventDefault()
+                if(CURRENT_PLAYER==X_PLAYER){
+                    CURRENT_PLAYER=O_PLAYER
+                }
+                else{
+                    CURRENT_PLAYER=X_PLAYER
+                }
             }
-            e.target.innerHTML=CURRENT_PLAYER
-            
-            if(CURRENT_PLAYER==X_PLAYER){
-                CURRENT_PLAYER=O_PLAYER
-            }
-            else{
-                CURRENT_PLAYER=X_PLAYER
-            }
-        }
+    }
+        })
     })
-})
-restartBtn.addEventListener("click",restart)
 function restart(){
+    header.innerHTML="TIC-TAC-TOE"
     cells.forEach((cell)=>{
         id=cell.id
         spaces[id]=null
         cell.innerHTML=''
     })
     CURRENT_PLAYER=X_PLAYER
+    gameOver=false
 }
 function playerHasWon(){
-    console.log('asdas',spaces[3],spaces[4],spaces[5],count)
     if(spaces[0]!=null &&((spaces[0]==spaces[1])&& (spaces[0]==spaces[2] ))||
        spaces[3]!=null &&((spaces[3]==spaces[4])&& (spaces[3]==spaces[5] ))||
        spaces[6]!=null &&((spaces[6]==spaces[7])&& (spaces[6]==spaces[8] ))||
