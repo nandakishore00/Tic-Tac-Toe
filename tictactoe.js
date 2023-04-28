6008d6c9ed974ae4b99c5dd009d8f57727cd9929
nandakishore reddy <nandakishorereddykamatham@gmail.com>
2023-04-28 23:34:53 +0530

    add strike-through for the winning row

--- tictactoe.js
+++ tictactoe.js
@@ -7,6 +7,9 @@ const restartBtn=document.querySelector(".restart");
 const cells = document.querySelectorAll(".cell")
 var spaces = new Array(9).fill(null);
 var gameOver=false
+var a=0
+var b=0
+var c=0
 const X_PLAYER='X'
 const O_PLAYER='O'
 let CURRENT_PLAYER='X'
@@ -26,6 +29,12 @@ cells.forEach((cell)=>{
                 count+=1
                 const playerWon=playerHasWon()
                 if(playerWon==true){
+                    var winningCombination=[a,b,c]
+                    const winningCell=winningCombination.map(index=>cells[index])
+                    winningCell.forEach(cell=>
+                    {
+                        cell.classList.add('winning-cell')
+                    })
                     e.target.innerHTML=CURRENT_PLAYER
                     gameOver=true
                     if(CURRENT_PLAYER=='X'){
@@ -52,6 +61,12 @@ cells.forEach((cell)=>{
 function restart(){
     header.innerHTML="TIC-TAC-TOE"
     count=0
+    var winningCombination=[a,b,c]
+    const winningCell=winningCombination.map(index=>cells[index])
+    winningCell.forEach(cell=>
+    {
+        cell.classList.remove('winning-cell')
+    })
     cells.forEach((cell)=>{
         id=cell.id
         spaces[id]=null
@@ -61,17 +76,55 @@ function restart(){
     gameOver=false
 }
 function playerHasWon(){
-    if(spaces[0]!=null &&((spaces[0]==spaces[1])&& (spaces[0]==spaces[2] ))||
-       spaces[3]!=null &&((spaces[3]==spaces[4])&& (spaces[3]==spaces[5] ))||
-       spaces[6]!=null &&((spaces[6]==spaces[7])&& (spaces[6]==spaces[8] ))||
-       spaces[0]!=null &&((spaces[0]==spaces[3])&& (spaces[0]==spaces[6] ))||
-       spaces[1]!=null &&((spaces[1]==spaces[4])&& (spaces[1]==spaces[7] ))||
-       spaces[2]!=null &&((spaces[2]==spaces[5])&& (spaces[2]==spaces[8] ))||
-       spaces[0]!=null &&((spaces[0]==spaces[4])&& (spaces[0]==spaces[8] ))||
-       spaces[2]!=null &&((spaces[2]==spaces[4])&& (spaces[2]==spaces[6]))){
+    if(spaces[0]!=null &&((spaces[0]==spaces[1])&& (spaces[0]==spaces[2] ))){
+        a=0
+        b=1
+        c=2
+        return true
+    }
+    else if(spaces[3]!=null &&((spaces[3]==spaces[4])&& (spaces[3]==spaces[5] ))){
+        a=3
+        b=4
+        c=5
+        return true
+    }
+    else if(spaces[6]!=null &&((spaces[6]==spaces[7])&& (spaces[6]==spaces[8] ))){
+        a=6
+        b=7
+        c=8
+        return true
+    }
+    else if(spaces[0]!=null &&((spaces[0]==spaces[3])&& (spaces[0]==spaces[6] ))){
+        a=0
+        b=3
+        c=6
+        return true
+    }
+    else if(spaces[1]!=null &&((spaces[1]==spaces[4])&& (spaces[1]==spaces[7] ))){
+        a=1
+        b=4
+        c=7
+        return true
+    }
+    else if(spaces[2]!=null &&((spaces[2]==spaces[5])&& (spaces[2]==spaces[8] ))){
+        a=2
+        b=5
+        c=8
+        return true
+    }
+    else if(spaces[0]!=null &&((spaces[0]==spaces[4])&& (spaces[0]==spaces[8] ))){
+        a=0
+        b=4
+        c=8
+        return true
+    }
+    else if(spaces[2]!=null &&((spaces[2]==spaces[4])&& (spaces[2]==spaces[6]))){
+        a=2
+        b=4
+        c=6
         return true
        }
-       else if(count==9)
+    else if(count==9)
        {
         return false 
        }
